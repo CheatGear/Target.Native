f06cd37c9b1c4bc30a3739486bd352fc25643b47
CorrM <islamnofl.official@gmail.com>
2022-03-04 06:20:12 +0200

    - Update to v3

--- Native.cs
+++ Native.cs
@@ -1,116 +1,178 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
 using CG.Framework.Attributes;
 using CG.Framework.Plugin.Memory;
 
 namespace CG.Memory;
 
-[Flags]
-internal enum ProcessAccessFlags : uint
-{
-    None = 0,
-    Terminate = 0x00000001,
-    CreateThread = 0x00000002,
-    VirtualMemoryOperation = 0x00000008,
-    VirtualMemoryRead = 0x00000010,
-    VirtualMemoryWrite = 0x00000020,
-    DuplicateHandle = 0x00000040,
-    CreateProcess = 0x000000080,
-    SetQuota = 0x00000100,
-    SetInformation = 0x00000200,
-    QueryInformation = 0x00000400,
-    QueryLimitedInformation = 0x00001000,
-    Synchronize = 0x00100000,
-    All = 0x001F0FFF
-}
-
 [PluginInfo("CorrM", "Native", "Use current system API to read/write memory process", "https://github.com/CheatGear", "https://github.com/CheatGear/Memory.Native")]
 public class Native : MemoryPlugin
 {
-    [DllImport("kernel32.dll", SetLastError = true)]
-    private static extern UIntPtr OpenProcess(ProcessAccessFlags processAccess, bool bInheritHandle, int processId);
-
-    [DllImport("kernel32.dll", SetLastError = true)]
-    private static extern bool ReadProcessMemory(UIntPtr hProcess, UIntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out int lpNumberOfBytesRead);
-
-    [DllImport("kernel32.dll", SetLastError = true)]
-    private static extern bool WriteProcessMemory(UIntPtr hProcess, UIntPtr lpBaseAddress, [In] byte[] lpBuffer, int dwSize, out int lpNumberOfBytesWritten);
-
-    [DllImport("kernel32.dll", SetLastError = true)]
-    private static extern bool IsWow64Process([In] UIntPtr processHandle, [Out] out bool wow64Process);
-
-    [DllImport("kernel32.dll", SetLastError = true)]
-    private static extern bool CloseHandle(UIntPtr hHandle);
+    private MemoryInitInfo _initInfo;
+    private IntPtr _pHandle;
+    private Win32.SystemInfo _sysInfo;
+    private int _memoryBasicInformationSize;
 
     public override Version TargetFrameworkVersion { get; } = new(3, 0, 0);
     public override Version PluginVersion { get; } = new(3, 0, 0);
-    public override UIntPtr ProcessHandle { get; protected set; }
     public override bool Is64Bit { get; protected set; }
-    public override bool IsInitialized { get; protected set; }
 
-    public static bool Is64BitProcess(UIntPtr processHandle)
+    private static bool Is64BitProcess(IntPtr processHandle)
     {
-        return IsWow64Process(processHandle, out bool retVal) && !retVal;
+        return Win32.IsWow64Process(processHandle, out bool retVal) && !retVal;
     }
 
-    public override bool Init(MemoryInitInfo info)
+    private UIntPtr GameStartAddress()
     {
-        ProcessHandle = OpenProcess(ProcessAccessFlags.All, false, info.Process.Id);
-        Is64Bit = Is64BitProcess(ProcessHandle);
+        return _sysInfo.MinimumApplicationAddress;
+    }
 
-        return IsInitialized = true;
+    private UIntPtr GameEndAddress()
+    {
+        return _sysInfo.MaximumApplicationAddress;
+    }
+
+    protected override bool OnInit(MemoryInitInfo info)
+    {
+        _initInfo = info;
+
+        _pHandle = Win32.OpenProcess(Win32.ProcessAccessFlags.All, false, info.Process.Id);
+        Is64Bit = Is64BitProcess(_pHandle);
+
+        _memoryBasicInformationSize = Marshal.SizeOf<Win32.MemoryBasicInformation>();
+        Win32.GetSystemInfo(out _sysInfo);
+
+        return true;
     }
 
     public override bool ReadBytes(UIntPtr address, int size, out byte[] buffer, out int numberOfBytesRead)
     {
         int cSize = size;
         var bytes = new byte[size];
-        bool success = false;
 
-        numberOfBytesRead = 0;
-        buffer = Array.Empty<byte>();
-
-        try
+        while (true)
         {
-            while (true)
+            bool success = Win32.ReadProcessMemory(_pHandle, address, bytes, cSize, out numberOfBytesRead);
+            if (!success)
             {
-                success = ReadProcessMemory(ProcessHandle, address, bytes, cSize, out numberOfBytesRead);
-                if (!success)
+                int error = Marshal.GetLastWin32Error();
+                if (error == 299 && numberOfBytesRead == 0 && cSize > 1) // ERROR_PARTIAL_COPY
                 {
-                    int error = Marshal.GetLastWin32Error();
-                    if (error == 299 && numberOfBytesRead == 0 && cSize > 1) // ERROR_PARTIAL_COPY
-                    {
-                        --cSize;
-                        continue;
-                    }
+                    --cSize;
+                    continue;
                 }
-                else
-                {
-                    buffer = bytes[..numberOfBytesRead];
-                }
-
-                return numberOfBytesRead == size && buffer.Length > 0;
             }
-        }
-        catch (Exception)
-        {
-            Console.WriteLine($"[ERR] Address = 0x{address.ToUInt64():X}, Size = {size}, NumberOfBytesRead = {numberOfBytesRead}.");
-            Console.WriteLine($"[ERR] BytesLen = {bytes.Length}, CSize = {cSize}, success = {success}");
-            throw;
+
+            buffer = bytes[0..numberOfBytesRead];
+            bool allRead = numberOfBytesRead == size && buffer.Length > 0;
+            numberOfBytesRead = cSize;
+            return allRead;
         }
     }
 
     public override bool WriteBytes(UIntPtr address, byte[] buffer, out int numberOfBytesWritten)
     {
-        WriteProcessMemory(ProcessHandle, address, buffer, buffer.Length, out numberOfBytesWritten);
+        Win32.WriteProcessMemory(_pHandle, address, buffer, buffer.Length, out numberOfBytesWritten);
         return numberOfBytesWritten == (uint)buffer.Length;
+    }
+
+    public override MemoryRegionInfo? GetMemoryRegion(UIntPtr address)
+    {
+        // Get Region information
+        bool valid = Win32.VirtualQueryEx(
+            _pHandle,
+            address,
+            out Win32.MemoryBasicInformation info,
+            (uint)_memoryBasicInformationSize
+        ) == _memoryBasicInformationSize;
+
+        if (!valid)
+            return null;
+
+        var region = new MemoryRegionInfo()
+        {
+            Address = info.BaseAddress,
+            Size = info.RegionSize.ToUInt64(),
+            State = (int)info.State,
+            Protect = (int)info.Protect,
+            Type = (int)info.Type,
+        };
+
+        return region;
+    }
+
+    public override List<MemoryModuleInfo> GetModuleList()
+    {
+        var ret = new List<MemoryModuleInfo>();
+        // To Avoid Some Games not share it's modules, or could be emulator game
+        try
+        {
+            IntPtr hSnap = Win32.CreateToolhelp32Snapshot(Win32.SnapshotFlags.Module | Win32.SnapshotFlags.Module32, _initInfo.Process.Id);
+            if (hSnap != Win32.InvalidHandleValue)
+            {
+                var modEntry = new Win32.ModuleEntry32()
+                {
+                    DwSize = (uint)Marshal.SizeOf(typeof(Win32.ModuleEntry32))
+                };
+
+                if (Win32.Module32First(hSnap, ref modEntry))
+                {
+                    do
+                    {
+                        ret.Add(modEntry);
+                    } while (Win32.Module32Next(hSnap, ref modEntry));
+                }
+            }
+            Win32.CloseHandle(hSnap);
+        }
+        catch
+        {
+            // Ignore
+        }
+
+        return ret;
+    }
+
+    public override bool IsBadAddress(UIntPtr uIntPtr)
+    {
+        return uIntPtr.ToUInt64() < GameStartAddress().ToUInt64() || uIntPtr.ToUInt64() > GameEndAddress().ToUInt64();
+    }
+
+    public override bool IsValidRemoteAddress(UIntPtr address)
+    {
+        // TODO: Very bad when called in hot-path
+        if (address == UIntPtr.Zero || IsBadAddress(address))
+            return false;
+
+        if (Win32.VirtualQueryEx(_pHandle, address, out Win32.MemoryBasicInformation info, (uint)_memoryBasicInformationSize) != 0)
+            return info.Protect != 0 && (info.Protect & Win32.MemoryProtection.PageNoAccess) == 0;
+
+        return false;
+    }
+
+    public override bool SuspendProcess()
+    {
+        return Win32.NtSuspendProcess(_pHandle) >= 0;
+    }
+
+    public override bool ResumeProcess()
+    {
+        return Win32.NtResumeProcess(_pHandle) >= 0;
+    }
+
+    public override bool TerminateProcess()
+    {
+        return Win32.NtTerminateProcess(_pHandle, 0) >= 0;
     }
 
     public override void Dispose()
     {
         base.Dispose();
 
-        CloseHandle(ProcessHandle);
+        Win32.CloseHandle(_pHandle);
         GC.SuppressFinalize(this);
     }
 }
